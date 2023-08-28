4a241914604b4cc871209092279b390ee575e4ea
Gabriel <ggrezzi@tlcorporate.com>
2023-08-28 15:53:12 -0300

    Actualizado para Primer-PreEntrega

--- PrimerEntrega/src/ProductManager.js
+++ PrimerEntrega/src/ProductManager.js
@@ -2,6 +2,9 @@
 //Inicializando el File System
 var fs = require('fs');
 const express=require('express')
+const path=require('path')
+
+let ruta=path.join(__dirname,'archivos','productos.json') 
 
 const app=express()
 const PORT=3000
@@ -10,40 +13,7 @@ app.listen(PORT,()=>{
     console.log(`server corriendo`)
 })
 
-//app.get('*',(req,res)=>{
-//    res.send("Execute Order 66")
-//})
-app.get('/products',(req,res)=>{
 
-    let filtros=Object.entries(req.query)
-    if (filtros.length>0){
-        if (filtros[0][0]=="limit") {
-            let limit=parseInt(filtros[0][1])
-            if (limit>testProductos.products.length){res.json(testProductos.products)}
-            else{
-                let resultados=[]
-                for (let index = 0; index < limit; index++) {
-                    resultados.push(testProductos.products[index])
-                    console.log(resultados)
-                }
-                res.json(resultados)
-            }
-        }
-        else{res.json("Invalid Parameter")} 
-    }
-    else{res.json(testProductos.getProducts())}
-    
-})
-app.get('/products/:pid',(req,res)=>{
-    let {pid} = req.params
-    console.log(pid, typeof pid)
-    idParam=parseInt(pid)
-    if(isNaN(idParam)){
-        res.json({status:'error', mensaje:'Require un argumento id de tipo numerico'})
-        return 
-    }
-    res.json(testProductos.getProductById(idParam))
-})
 
 class ProductManager {
 
@@ -124,8 +94,6 @@ class ProductManager {
         });
         if (isPresent) {return this.products[location]}
         return ("Not Found")
-        
-
     }
 
     updateProduct(id,tempProd){
@@ -179,16 +147,4 @@ class ProductManager {
 
 }
 
-
-//Codigo para testeo de la clase y sus propiedades
-console.log("Creo mi instancia testPRoductos")
-let testProductos = new ProductManager("C:/gabriel/TEST.json");
-//testProductos.getProducts()
-//testProductos.addProduct("producto prueba","Esto es un prod prueba", 20, "sin imagen", "codigo1", 23)
-//testProductos.addProduct("producto prueba 2","Esto es un prod prueba 2", 21, "sin imagen", "codigo2", 23)
-//console.log(testProductos.getProducts())
-//console.log(testProductos.addProduct("producto prueba","Esto es un prod prueba", 20, "sin imagen", "codigo1", 23))
-//console.log(testProductos.getProductById(1))
-//console.log(testProductos.getProductById(3))
-//console.log(testProductos.deleteProduct(1))
 
